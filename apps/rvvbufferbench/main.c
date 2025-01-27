2a532ada7196f5593dacf5288a3c7aca1ef2867d
gjwverweij <g.j.w.verweij@tudelft.nl>
2025-01-27 16:53:55 +0100

    rvv buffer benchmark

---
+++ apps/rvvbufferbench/main.c
@@ -0,0 +1,42 @@
+int main() {
+    int img_height = 6;
+    int img_width = 32;
+    float input[img_height][img_width];
+
+    for(int y=0;y<img_height;y++){
+        for(int x=0;x<img_width;x++){
+            input[y][x] = (float)((float)y*img_width)+(float)x;
+        }
+    }
+
+
+    asm volatile("vsetvli zero, %0, e32, m2, ta, ma" :: "r" (32));
+    asm volatile("vle32.v v0,(%0);" :: "r"(&input[0][0]));
+    asm volatile("FENCE");
+    asm volatile("FENCE");
+    asm volatile("FENCE");
+
+    // single reg test
+    // asm volatile("vle32.v v2,(%0);" :: "r"(&input[1][0]));
+    // asm volatile("vfadd.vv v0,v2,v0");
+    // asm volatile("vle32.v v2,(%0);" :: "r"(&input[2][0]));
+    // asm volatile("vfadd.vv v0,v2,v0");
+    // asm volatile("vle32.v v2,(%0);" :: "r"(&input[3][0]));
+    // asm volatile("vfadd.vv v0,v2,v0");
+    // asm volatile("vle32.v v2,(%0);" :: "r"(&input[4][0]));
+    // asm volatile("vfadd.vv v0,v2,v0");
+    // asm volatile("vle32.v v2,(%0);" :: "r"(&input[5][0]));
+    // asm volatile("vfadd.vv v0,v2,v0");
+
+    // double reg test
+    asm volatile("vle32.v v2,(%0);" :: "r"(&input[1][0]));
+    asm volatile("vfadd.vv v0,v2,v0");
+    asm volatile("vle32.v v4,(%0);" :: "r"(&input[2][0]));
+    asm volatile("vfadd.vv v0,v4,v0");
+    asm volatile("vle32.v v2,(%0);" :: "r"(&input[3][0]));
+    asm volatile("vfadd.vv v0,v2,v0");
+    asm volatile("vle32.v v4,(%0);" :: "r"(&input[4][0]));
+    asm volatile("vfadd.vv v0,v4,v0");
+    asm volatile("vle32.v v2,(%0);" :: "r"(&input[5][0]));
+    asm volatile("vfadd.vv v0,v2,v0");
+}
